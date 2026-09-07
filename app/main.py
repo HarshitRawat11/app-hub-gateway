7c527118b869916e01cc3cf12b9c736206d028cf
Harshit Rawat <harshitrawat2011@gmail.com>
2026-09-07 11:27:49 +0530

    Wrote async getLinks function

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import httpx
 
 app = FastAPI()
 
 @app.get("/health")
 def health():
     return {"status":'ok'}
+
+@app.get("/links")
+async def getLinks():
+    async with httpx.AsyncClient() as client:
+        response = await client.get("http://localhost:8000/links")
+        return response.json()
+
